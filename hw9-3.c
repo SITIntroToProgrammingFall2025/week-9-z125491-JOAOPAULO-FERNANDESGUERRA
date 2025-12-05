2a2c0e056da6e8f80fee873779951fa118f9021f
z125491-JOAO PAULO-FERNANDES GUERRA <z125491@shibaura-it.ac.jp>
2025-12-05 14:54:16 +0900

    Update hw9-3.c

--- hw9-3.c
+++ hw9-3.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+int main() {
+
+    float A[3][2], B[2][3], C[3][2];
+    int i, j, k;
+
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 2; j++) {
+            scanf("%f", &A[i][j]);
+        }
+    }
+
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 3; j++) {
+            scanf("%f", &B[i][j]);
+        }
+    }
+
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 2; j++) {
+            C[i][j] = 0;
+            for(k = 0; k < 2; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    printf("The first matrix you entered is\n");
+    printf("%g %g %g\n", A[0][0], A[0][1], A[0][2]);
+}
 

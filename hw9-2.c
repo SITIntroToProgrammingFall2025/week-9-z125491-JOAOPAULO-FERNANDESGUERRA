d7a39cbd4e906e76aa111c2c4dab1e95ce1f0b31
z125491-JOAO PAULO-FERNANDES GUERRA <z125491@shibaura-it.ac.jp>
2025-12-05 14:51:38 +0900

    Update hw9-2.c

--- hw9-2.c
+++ hw9-2.c
@@ -23,12 +23,14 @@ int main()
         printf("\n");
     }
 
-    printf("\nOutput\n");
+    printf("Output\n");
+
     for (i = 0; i < 3; i++)
     {
-        printf("%d %d %d\n", a[i][2], a[i][1], a[i][0]);
+        printf("%d %d %d \n", a[i][2], a[i][1], a[i][0]);
     }
 
     return 0;
 }
+
 

adaaa5af33f32cf3e6d3f489ac096865eb886be6
z125491-JOAO PAULO-FERNANDES GUERRA <z125491@shibaura-it.ac.jp>
2025-12-05 15:04:49 +0900

    Update ahw9-2.c

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+    int a[5];
+    int i, j, temp;
+
+    for(i = 0; i < 5; i++)
+        scanf("%d", &a[i]);
+
+    for(i = 0; i < 5; i++) {
+        for(j = i + 1; j < 5; j++) {
+            if(a[j] < a[i]) {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+    printf("sorted array:\n");
+    for(i = 0; i < 5; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
 

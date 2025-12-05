fbf64712b7f560b0ffd7735df2df78ba8d098ecf
z125491-JOAO PAULO-FERNANDES GUERRA <z125491@shibaura-it.ac.jp>
2025-12-05 14:47:40 +0900

    Update hw9-2.c

--- hw9-2.c
+++ hw9-2.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[3][3];
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("You entered\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nOutput\n");
+    for (i = 0; i < 3; i++)
+    {
+        printf("%d %d %d\n", a[i][2], a[i][1], a[i][0]);
+    }
+
+    return 0;
+}
 
